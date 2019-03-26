b631f802d6629f33c79ecd4c5570930df3e25110
Sebastian S <s.stickel@gmx.de>
2019-03-27 00:27:53 +0100

    reduce speed between probes - G29 no longer measuring bullshit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -798,7 +798,7 @@
 #define MIN_PROBE_EDGE 60
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 13000
+#define XY_PROBE_SPEED 4000
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
