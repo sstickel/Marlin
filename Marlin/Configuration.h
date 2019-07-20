4203026ba132a25355680910e2a77e0139380bc9
Sebastian S <s.stickel@gmx.de>
2019-07-20 20:35:09 +0200

    Now it works on XYZZ2, yess

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1792,7 +1792,7 @@
 //
 // ANET and Tronxy 20x4 Controller
 //
-#define ZONESTAR_LCD              // Requires ADC_KEYPAD_PIN to be assigned to an analog pin.
+//#define ZONESTAR_LCD              // Requires ADC_KEYPAD_PIN to be assigned to an analog pin.
                                   // This LCD is known to be susceptible to electrical interference
                                   // which scrambles the display.  Pressing any button clears it up.
                                   // This is a LCD2004 display with 5 analog buttons.
